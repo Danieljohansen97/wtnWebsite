61d49bc70da846e3cf5d6346842dd7484775a883
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-03-18 14:11:56 +0100

    Add email form and email validation

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function showNav() {
     x.style.display = "inline-block";
   }
 }
+
+
+/* Eventlistener that checks if email has the right syntax */
+var mail;
+window.onload=function(){
+    mail = document.getElementById("mail");
+    mail.addEventListener("input", validateEmail);
+}
+function validateEmail(){
+    mail = document.getElementById("mail").value;
+    if(mail.includes("@" && ".")){
+        document.getElementById("mail").style.backgroundColor = "rgba(0, 255, 0, 0.5)";
+    }else{
+        document.getElementById("mail").style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    }
+}
